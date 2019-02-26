476288d3771ec2cc5ecf24cee38aed481a16a5bb
jshli <joshua.li@mu-aa.com>
2019-02-27 09:57:36 +1100

    asych task drawing

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -7,17 +7,8 @@
 //     }, 1000);
 // }
 
-const taskItem = document.querySelectorAll('.task-item--wrap')
-const header = document.querySelector('.section__header');
+// const header = document.querySelector('.section__header');
 
-const editShift = event => {
-
-    if (event.target.classList.contains('task-item__details')){
-        event.target.classList.toggle('task-item__details--shrink');
-    }
-
-    console.log(event.target)
-};
 
 // var swipe = new Hammer(header);
 
@@ -25,5 +16,8 @@ const editShift = event => {
 // 	console.log("hello");
 // });
 
-taskItem.forEach(el => el.addEventListener('click', editShift));
+// taskItem.forEach(el => el.addEventListener('click', editShift));
 
+// $( function() {
+//     $( "#datepicker" ).datepicker();
+//   } );
